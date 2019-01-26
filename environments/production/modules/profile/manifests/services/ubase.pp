72d6f84a090dee14f013f106194dfbb5dbc99371
rihaz jerrin <rihaz.jerrin@gmail.com>
2019-01-26 15:06:13 +0530

    nfs client

--- environments/production/modules/profile/manifests/services/ubase.pp
+++ environments/production/modules/profile/manifests/services/ubase.pp
@@ -5,8 +5,9 @@ class profile::services::ubase (
   Hash  $sysctl = lookup('sysctl',  { value_type => Hash } ),
   Array $commands = lookup('commands', {value_type => Array}),
   Array $packages = lookup('packages', {value_type => Array, default_value => [] }),
-
-
+  Array $ipackages = lookup('ipackages', {value_type => Array, default_value => [] }),
+  Hash  $files  = lookup('files',  { value_type => Hash, default_value => {} } ),
+  Hash  $fcontent  = lookup('fcontent',  { value_type => Hash, default_value => {} } ),
 )
 {
 
@@ -18,69 +19,95 @@ class profile::services::ubase (
         
       }
 
+    }
 
+    $ipackages.each | String $pkg | {
+      package { $pkg:
+        name => $pkg,
+        ensure => present,
+        
 
       }
-    
-        
+    }
     include  'profile::services::upgrade'
     include  'profile::services::time'	
-
 }
 
-  network_config { $net['interface'] :
-    ensure    => present,
-    family    => 'inet',
-    ipaddress => $net['IP'],
-    method    => 'static',
-    netmask   => $net['netmask'],
-    onboot    => true,
-  }
+ 
+network_config { $net['interface'] :
+  ensure    => present,
+  family    => 'inet',
+  ipaddress => $net['IP'],
+  method    => 'static',
+  netmask   => $net['netmask'],
+  onboot    => true,
+}
   
-  network_route { 'default':
-    ensure    => present,
-    gateway   => $net['gateway'],
-    netmask   => '0.0.0.0',
-    network   => 'default'
-  }
+network_route { 'default':
+  ensure    => present,
+  gateway   => $net['gateway'],
+  netmask   => '0.0.0.0',
+  network   => 'default'
+}
 
-  if  $facts['os']['name'] == 'Debian'  {
-   
-    $commands.each  | String $cmd |  {
+if  $facts['os']['name'] == 'Debian'  {
+  $commands.each  | String $cmd |  {
     exec { "${cmd}":
       path => ['/sbin','/usr/bin', '/home/jery',],
       user => 'root',
       
-
     }
+
+  }
+}
     
-    }
-  }
-  
-
-  class { 'resolv_conf':
-    nameservers =>  $ns,
-  }
+ 
+class { 'resolv_conf':
+  nameservers =>  $ns,
+}
                      
-
-  $sysctl.each | String $sysvar, String $value | {
-    sysctl { $sysvar:
-      ensure => present,
-      value =>  $value,
-    }
-
+$sysctl.each | String $sysvar, String $value | {
+  sysctl { $sysvar:
+    ensure => present,
+    value =>  $value,
   }
 
-  $packages.each | String $package | {
-    package { $package:
-      name => $package,
-      ensure => absent,
-    }
+}
 
-    }
+
+$packages.each | String $package | {
+  package { $package:
+    name => $package,
+    ensure => absent,
+  }
+}
+
+$files.each | String $path, String $ftype | {
+  file { "${path}":
+    path  =>  $path,
+    ensure => $ftype,
+    owner  => 'root',
+    mode  => '755',
+    
+  }
+  
+}
+
+$fcontent.each | String $path, String $content | {
+  file_line { 'fcontent':
+    ensure => present,
+    path   => $path,
+    line   => $content,
+    match  => "^${content}",
+  }
+  
+}
+
+}
   
 
-  }
+
+ 
 
 
         
