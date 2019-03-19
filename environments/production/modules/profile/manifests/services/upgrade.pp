3cbfb3157765855fd1f91c880e15b469e27808a8
rihaz jerrin <rihaz.jerrin@gmail.com>
2019-03-19 10:01:27 +0530

    updating crontab

--- environments/production/modules/profile/manifests/services/upgrade.pp
+++ environments/production/modules/profile/manifests/services/upgrade.pp
@@ -2,7 +2,7 @@ class profile::services::upgrade () {
 cron { 'upgrade':
   command => 'apt-get -y  update  && apt-get -y dist-upgrade',
   user    => 'root',
-  hour    => 18,
+  hour    => 11,
   minute  => 35,
 }
 }
